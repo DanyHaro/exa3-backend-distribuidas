5ba7cdc60166c740e45f1d079002b120dee27285
unknown <danyharo@upeu.edu.pe>
2021-07-18 22:51:32 -0500

    make it better

--- src/controllers/archivo.controller.js
+++ src/controllers/archivo.controller.js
@@ -7,7 +7,7 @@ export const getAllarchivo = async (req, res) => {
         return res.status(200).json(response.rows);
     } catch (e) {
         console.log(e);
-        return res.status(500).json('Error Interno....!');
+        return res.status(500).json('Error Interno.....!');
     }
 }
 
